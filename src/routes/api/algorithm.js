c05f247ceeea2efcd7afc553b96c1ce1a06c230b
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-14 13:34:44 -0600

    Remove client id from algorithm summary endpoint

--- src/routes/api/algorithm.js
+++ src/routes/api/algorithm.js
@@ -7,4 +7,15 @@ const algorithmController = require('../../controllers/api/algorithm')
 
 router.post('/result', algorithmController.getAlgorithmResult)
 
+/**
+ * @swagger
+ * /api/algorithm/summary:
+ *   get:
+ *     summary: Obtiene un resumen de los valores usados en el algoritmo
+ *     responses:
+ *       200:
+ *         description: Resumen generado
+ */
+router.get('/summary', algorithmController.getAlgorithmSummary)
+
 module.exports = router
