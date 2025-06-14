951f1d1c6b8b1a1381f07b6ebca9ac186f1804e3
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-14 14:19:47 -0600

    Add PDF report for algorithm summary

--- src/routes/api/algorithm.js
+++ src/routes/api/algorithm.js
@@ -7,4 +7,26 @@ const algorithmController = require('../../controllers/api/algorithm')
 
 router.post('/result', algorithmController.getAlgorithmResult)
 
+/**
+ * @swagger
+ * /api/algorithm/summary:
+ *   get:
+ *     summary: Obtiene un resumen de los valores usados en el algoritmo
+ *     responses:
+ *       200:
+ *         description: Resumen generado
+ */
+router.get('/summary', algorithmController.getAlgorithmSummary)
+
+/**
+ * @swagger
+ * /api/algorithm/summary/pdf:
+ *   get:
+ *     summary: Genera un reporte PDF con los valores del algoritmo
+ *     responses:
+ *       200:
+ *         description: PDF generado en base64
+ */
+router.get('/summary/pdf', algorithmController.getAlgorithmSummaryPdf)
+
 module.exports = router
