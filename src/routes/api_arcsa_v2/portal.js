1c6e629713053c26542cc7fa9eed7945ba6eade9
Minoru Daniel Salazar Palafox <minorusal@hotmail.com>
2025-06-30 11:16:23 -0600

    feat: auto-generate swagger docs

--- src/routes/api_arcsa_v2/portal.js
+++ src/routes/api_arcsa_v2/portal.js
@@ -3,6 +3,27 @@
 const express = require('express');
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api_arcsa_v2/portal/nueva-ruta:
+ *   post:
+ *     summary: Ejemplo de ruta nueva
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *               content:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Respuesta exitosa
+ */
+
 router.post('/nueva-ruta', (req, res) => {
   const { title, content } = req.body;
   res.json({ success: true, message: 'nueva ruta a api', data: { title, content } });
